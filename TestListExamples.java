d3011ed7a15a563df78f1992ea7bfd1374991528
Bran Zanger <brandon_z_zhang@Yahoo.com>
2023-11-29 14:39:53 -0800

    Added more tests for merge and filter

--- TestListExamples.java
+++ TestListExamples.java
@@ -9,6 +9,12 @@ class IsMoon implements StringChecker {
 	}
 }
 
+class IsA implements StringChecker {
+	public boolean checkString(String s) {
+		return s.equalsIgnoreCase("A");
+	}
+}
+
 public class TestListExamples {
 	
 	
@@ -18,9 +24,38 @@ public class TestListExamples {
 		List<String> right = Arrays.asList("a", "d");
 		List<String> merged = ListExamples.merge(left, right);
 		List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+
 		assertEquals(expected, merged);
-		
-		
-		
 	}
+
+	@Test(timeout = 500)
+	public void testMergeLeftEnd(){
+		List<String> left = Arrays.asList("a", "b", "c", "e");
+		List<String> right = Arrays.asList("a", "c", "d");
+		List<String> merged = ListExamples.merge(left, right);
+		List<String> expected = Arrays.asList("a", "a", "b", "c", "c", "d", "e");
+
+		assertEquals(expected, merged);
+	}
+
+	@Test(timeout = 500)
+	public void testFilterA(){
+		List<String> input = Arrays.asList("a", "b", "c", "A", "moon", "a", "7", "MOON", "*");
+		List<String> output = ListExamples.filter(input, new IsA());
+		String[] expected = {"a", "A", "a"};
+
+		assertArrayEquals(expected, output.toArray());
+	}
+
+	@Test(timeout = 500)
+	public void testFilterMoon(){
+		List<String> input = Arrays.asList("a", "b", "c", "A", "moon", "a", "7", "MOON", "*");
+		List<String> output = ListExamples.filter(input, new IsMoon());
+		String[] expected = {"moon", "MOON"};
+
+		assertArrayEquals(expected, output.toArray());
+	}
+
+
+
 }
